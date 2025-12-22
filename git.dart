447ba8c12758509b3c939a803f0525733eefda78
kritik khodiyar <khodiyarkritik@zohomail.in>
2025-12-22 11:56:27 +0530

    version 2

--- git.dart
+++ git.dart
@@ -1,3 +1,12 @@
 void main() {
-  print('Hello, World!');
+  print('Hello, World!') ;
+print("calculate the sum");
+int sum = 0;
+int a=23;
+int b=45;sum = a + b;
+print("the sum is $sum");
+
+
+
+
 }
