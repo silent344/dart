afc06321495153a86020b978cb72dcda5f531a25
silent344 <khodiyarkritik@zohomail.in>
2025-12-23 12:37:30 +0530

    variable

---
+++ variable.dart
@@ -0,0 +1,25 @@
+// a variable
+
+void main(){
+// valid variable name
+var Fname= 'locky';
+var namee ="raghav";
+var money = 200;
+// invalid variable name
+/* 
+var 1name = " motu";
+var for = "patlu";
+*/
+// print values
+print(Fname);
+print(namee);
+print(money);
+
+
+
+
+
+
+
+
+}
