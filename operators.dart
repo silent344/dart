80bc0610dc6f378f11d82888d7d576e073d8ef7c
silent344 <khodiyarkritik@zohomail.in>
2025-12-23 19:49:43 +0530

    operators

---
+++ operators.dart
@@ -0,0 +1,47 @@
+void main(){
+// Arithmetic operators
+var a =340;
+var b=34;
+var c= 5;
+print("value : ${(a+b)*c}");
+print(a+b);
+print(b-a);
+print(a*b);
+print(a/b);// give value in double
+print(a~/b); // give an exact value
+print(a%b); // give us remainder
+// prifix and postfi
+print("--------------------------------");
+int i=1;
+int k=2;
+int j=3;
+print("values of ++i:${++i}");//2 First increase than print
+print("values of i++:${i++}");//2  first print than increase
+print(i);//3
+print("values of i--:${i--}");//3 First print than decrease
+print(i);//2
+print("values of --i:${--i}");//1 First derease than print
+
+print("K block --------");
+print("values of ++k:${++k}");//3
+print("values of k++:${k++}");//3
+print(k);//4
+print("values of k--:${k--}");//4
+print(k);//3
+print("values of --k:${--k}");//2
+
+print("--------------");
+print("values of ++j:${++j}");
+print("values of j++:${j++}");
+print(j);
+print("values of j--:${j--}");
+print(j);
+print("values of --j:${--j}");
+
+
+
+
+
+
+
+}
