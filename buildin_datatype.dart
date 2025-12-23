f8988876d6d50beee2f453d8b4cb1fafb1acbbcb
silent344 <khodiyarkritik@zohomail.in>
2025-12-23 13:33:12 +0530

    variable and datatype

---
+++ buildin_datatype.dart
@@ -0,0 +1,31 @@
+void main(){
+  //bulid in datatype
+  int vegetable =450;
+  int fruits =235;
+  int species =240;
+int sum = vegetable+fruits+species;
+print("sum or groceries ${sum}");
+// double
+double vegies =900.453;
+double fruities =30.684;
+double masale =3554.892;
+double sum2 =masale+vegies+fruities;
+print("sum of groceries in points or paisa = ${sum2}");
+//string
+String City_name="Dhanbad";
+String state_name ="Jharkhand";
+
+print(City_name);
+print(state_name);
+// boolean
+bool is_true= true;
+bool is_false = false;
+print(is_true);
+print(is_false);
+// var work dynamically
+var passing_marks = 40;
+print(passing_marks);
+print("It show the type of data: ${passing_marks.runtimeType}");
+// var change datatype according value
+
+}
